1a2535d9e2830d9ad27c2a2a9cacbdb91fedbcf4
Liam Clink <clink.6@osu.edu>
2020-01-13 23:24:00 -0500

    Added Simulation class

--- main.cpp
+++ main.cpp
@@ -6,24 +6,39 @@
 
 struct Particle
 {
-    double charge_mass_ratio;
+    double charge, mass;
     std::vector<double, 2> position;
     std::vector<double, 2> velocity;
 
-    Particle(double cm_ratio, std::vector<double, 2> pos, std::vector<double, 2> vel)
+    Particle(double c, double m, std::vector<double, 2> pos, std::vector<double, 2> vel)
     {
-        charge_mass_ratio = cm_ratio;
+        charge = c;
+	mass = m;
         position = pos;
         velocity = vel;
     }
 };
 
+// We need the vapor particles to be polarizable, as well as having a charge
 struct PolarParticle : public Particle
 {
     std::vector<double, 2> dipole_moment;
 
     PolarParticle(std::vector<double, 2> dipole) : dipole_moment{dipole}
 };
+
+class Simulation
+{
+	Simulation(double x_length, double y_length, double dx, double dy,
+			double charge_density, double vapor_density);
+	double x_length, y_length;
+	double dx, dy;
+	double charge_density, vapor_density;
+
+	std::vector<Particle> ions;
+	std::vector<Particle> electrons;
+	std::vector<PolarParticle> vapor;
+}
 
 int main()
 {
